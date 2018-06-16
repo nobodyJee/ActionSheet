d15412c98cac63c1a3f5e04fbc82cf9d75f7df38
姬武超 <1500536442@qq.com>
2018-06-16 18:08:01 +0800

    更新文件

--- JWCActionSheet/JWCActionSheet/AppDelegate.swift
+++ JWCActionSheet/JWCActionSheet/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  ActionSheet
+//  JWCActionSheet
 //
 //  Created by JiWuChao on 2018/6/16.
 //  Copyright © 2018年 JiWuChao. All rights reserved.
--- JWCActionSheet/JWCActionSheet/ViewController.swift
+++ JWCActionSheet/JWCActionSheet/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  ActionSheet
+//  JWCActionSheet
 //
 //  Created by JiWuChao on 2018/6/16.
 //  Copyright © 2018年 JiWuChao. All rights reserved.
